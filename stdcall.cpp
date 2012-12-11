f51054653bd63c668a18024fc500ea7ae12e57fc
timurrrr@google.com <timurrrr@google.com@f3716982-0670-aa43-fdc7-53a2bea90ebe>
2012-12-11 14:16:36 +0000

    One more __stdcall function call

--- stdcall.cpp
+++ stdcall.cpp
@@ -1,9 +1,14 @@
 extern int cnt;
 void __stdcall foo();
+extern "C" void __stdcall bar();
 
 #ifdef CONFIG_1
 void __stdcall foo() {
   cnt = 42;
+}
+
+void __stdcall bar() {
+  cnt = 13;
 }
 
 #else
@@ -11,6 +16,11 @@ int cnt = 10;
 
 int main() {
   foo();
-  return (cnt == 42) ? 0 : 1;
+  if (cnt != 42)
+    return 1;
+
+  bar();
+  if (cnt != 13)
+    return 2;
 }
 #endif
