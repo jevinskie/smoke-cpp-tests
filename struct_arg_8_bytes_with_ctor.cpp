a12ef9f3271f908524ee15591064d364d8f4ff3c
timurrrr@google.com <timurrrr@google.com@f3716982-0670-aa43-fdc7-53a2bea90ebe>
2013-03-27 01:13:25 +0000

    More arg tests

---
+++ struct_arg_8_bytes_with_ctor.cpp
@@ -0,0 +1,23 @@
+// http://llvm.org/PR13676
+#include "common.h"
+
+struct S {
+  S() {}
+  int a, b;
+};
+
+void foo(S s);
+
+#ifdef CONFIG_1
+void foo(S s) {
+  CHECK_EQ(1, s.a);
+  CHECK_EQ(2, s.b);
+}
+#else
+int main() {
+  S s;
+  s.a = 1;
+  s.b = 2;
+  foo(s);
+}
+#endif
