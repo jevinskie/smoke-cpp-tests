4b44ca594515f5adc1ae85f458fb1f4cc6512602
rnk@google.com <rnk@google.com@f3716982-0670-aa43-fdc7-53a2bea90ebe>
2013-10-10 20:41:39 +0000

    Add a test for deleting a class with standard diamond inheritance
    This was reduced from gtest's Message class which uses a
    scoped_ptr<std::stringstream>.  A larger reduction is:
    
      #include <sstream>
      int main() {
        std::stringstream *ss = new std::stringstream();
        delete ss; // BOOM
      }

---
+++ vbase_vdtors_diamond.cpp
@@ -0,0 +1,48 @@
+#include "common.h"
+
+struct A {
+  virtual ~A();
+};
+struct B : virtual A {
+  virtual ~B();
+};
+struct C : virtual A {
+  virtual ~C();
+};
+struct D : B, C {
+  virtual ~D();
+};
+
+extern int a_calls;
+extern int b_calls;
+extern int c_calls;
+extern int d_calls;
+
+void destroy(D *d);
+
+#ifdef CONFIG_1
+int a_calls;
+int b_calls;
+int c_calls;
+int d_calls;
+A::~A() { a_calls++; }
+B::~B() { b_calls++; }
+C::~C() { c_calls++; }
+D::~D() { d_calls++; }
+
+void destroy(D *d) {
+  // segfaults, clang generates bad code, assumes vftable is at offset zero or
+  // something.
+  delete d;
+}
+#else
+
+int main() {
+  D *d = new D();
+  destroy(d);
+  CHECK_EQ(1, a_calls);
+  CHECK_EQ(1, b_calls);
+  CHECK_EQ(1, c_calls);
+  CHECK_EQ(1, d_calls);
+}
+#endif
