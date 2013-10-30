3f70ef8a608d3852c74c7846f33abe247040bfd9
timurrrr@google.com <timurrrr@google.com@f3716982-0670-aa43-fdc7-53a2bea90ebe>
2013-10-30 15:27:33 +0000

    Add yet another vtordisp test

--- vbase_vtordisp_crash.cpp
+++ vbase_vtordisp_crash.cpp
@@ -1,20 +1,26 @@
 #include "common.h"
 
+struct X {
+  virtual void x() {}
+};
+
 struct A {
   A();
   ~A();
-  virtual void c();
-  virtual void d();
+  virtual void c(void*);
+  virtual void d(void*);
   int stage_a;
 };
 
-struct B : virtual A {
+struct Y : X, A {};
+
+struct B : virtual Y {
   B();
   ~B();
   // B's thunks use static this adjustment, assuming A's layout follows the 
   // members of B.
-  virtual void c();
-  virtual void d();
+  virtual void c(void*);
+  virtual void d(void*);
   int stage_b;
 };
 
@@ -24,40 +30,50 @@ struct B : virtual A {
 struct C : virtual B {
   C();
   ~C();
+  void call_c();
+};
+
+struct Z { int z; };
+
+struct D : virtual Y, virtual Z, C {
 };
 
 #ifdef CONFIG_1
 A::A() {
   stage_a = 1;
-  c();
+  c(this);
 }
 
 B::B() {
   CHECK_EQ(1, stage_a);
   stage_a = 2;
   stage_b = 1;
-  c();
+  c(this);
 }
 
 // Force ctor and dtor to be compiled in different TUs. 
 C::C() {
   CHECK_EQ(2, stage_a);
   CHECK_EQ(1, stage_b);
-  c();
+  call_c();
   stage_a = 3;
   stage_b = 2;
+}
+
+void C::call_c() {
+  c((B*)this);
 }
 
 B::~B() {
   CHECK_EQ(4, stage_a);
   CHECK_EQ(3, stage_b);
-  d();
+  d(this);
   stage_a = 5;
 }
 
 A::~A() {
   CHECK_EQ(5, stage_a);
-  d();
+  d(this);
 }
 
 #else
@@ -68,24 +84,33 @@ C::~C() {
   CHECK_EQ(2, stage_b);
   stage_a = 4;
   stage_b = 3;
-  d();
+  d((B*)this);
 }
 
-void A::c() { CHECK_EQ(1, stage_a); }
+void A::c(void *p) {
+  CHECK_EQ(this, p);
+  CHECK_EQ(1, stage_a);
+}
 
-void B::c() {
+void B::c(void *p) {
+  CHECK_EQ(this, p);
   CHECK_EQ(2, stage_a);
   CHECK_EQ(1, stage_b);
 }
 
-void B::d() {
+
+void B::d(void *p) {
+  CHECK_EQ(this, p);
   CHECK_EQ(4, stage_a);
   CHECK_EQ(3, stage_b);
 }
 
-void A::d() { CHECK_EQ(5, stage_a); }
+void A::d(void *p) {
+  CHECK_EQ(this, p);
+  CHECK_EQ(5, stage_a);
+}
 
 int main() {
-  C obj;
+  D obj;
 }
 #endif
